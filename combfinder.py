4ce7e80a5a656e550b9b5a714b995af45e68e4d0
jquentin-lion <jeremy.quentin@lionstudios.cc>
2021-12-22 17:42:52 +0100

    Class Bee

--- combfinder.py
+++ combfinder.py
@@ -15,8 +15,22 @@ def load_words():
     
 english_words = load_words()
 already_used = set()
+
+class Bee:
+    letters: set
+    pangram: str
+    other_words: set
     
-def is_valid(word):
+    def __init__(self, letters: set, pangram: str, other_words: set):
+        self.letters = letters
+        self.pangram = pangram
+        self.other_words = other_words
+    
+    def __str__(self):
+        return f"{''.join(self.letters)} -> {len(self.other_words)+1}: {self.pangram},{','.join(self.other_words)}"
+        
+    
+def get_bee(word):
     has_7_letters = len(word) >= UNIQUE_LETTERS_COUNT and len(''.join(set(word))) == UNIQUE_LETTERS_COUNT
     if has_7_letters:
         other_words = set()
@@ -24,24 +38,24 @@ def is_valid(word):
             if w not in already_used and w != word and set(w).issubset(set(word)):
                 other_words.add(w)
         if 10 <= len(other_words) <= 30:
-            return (True, other_words)
-    return (False, set())
+            return Bee(set(word), word, other_words)
+    return None
     
 
 
-fitting_words = []
+bees = []
 
 for word in english_words:
-    (valid, others) = is_valid(word)
-    if valid:
-        fitting_words.append(f"{''.join(set(word))} -> {len(others)+1}: {word},{','.join(others)}")
+    bee = get_bee(word)
+    if bee is not None:
+        bees.append(bee)
         already_used.add(word)
 
 
 #fitting_words = list(filter(is_valid, english_words))
-print(f"{len(fitting_words)} words with {UNIQUE_LETTERS_COUNT} unique letters:")
+print(f"{len(bees)} words with {UNIQUE_LETTERS_COUNT} unique letters:")
 
-pydoc.pager('\n'.join(fitting_words))
+pydoc.pager('\n'.join(str(b) for b in bees))
 
 #    for i in range(0, 100):
 #        print(f"{fitting_words[i]} -> {''.join(set(fitting_words[i]))}")
