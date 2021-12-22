914e99afa51aa6d36a5dc734793d08a60811e184
jquentin-lion <jeremy.quentin@lionstudios.cc>
2021-12-22 17:50:16 +0100

    Clean code

--- combfinder.py
+++ combfinder.py
@@ -11,10 +11,6 @@ def load_words():
         valid_words = word_file.read().split()
 
     return [word for word in valid_words if len(word) >= 3]
-    
-    
-english_words = load_words()
-already_used = set()
 
 class Bee:
     letters: set
@@ -25,35 +21,36 @@ class Bee:
         self.letters = letters
         self.pangram = pangram
         self.other_words = other_words
+        
+    @staticmethod
+    def create_bee(word: str):
+        has_7_letters = len(word) >= UNIQUE_LETTERS_COUNT and len(''.join(set(word))) == UNIQUE_LETTERS_COUNT
+        if has_7_letters:
+            other_words = set()
+            for w in english_words:
+                if w != word and set(w).issubset(set(word)):
+                    other_words.add(w)
+            if 10 <= len(other_words) <= 30:
+                return Bee(set(word), word, other_words)
+        return None
+                
+    @staticmethod
+    def create_bees(words):
+        bees = []
+        for word in words:
+            bee = Bee.create_bee(word)
+            if bee is not None:
+                bees.append(bee)
+        return bees
     
     def __str__(self):
         return f"{''.join(self.letters)} -> {len(self.other_words)+1}: {self.pangram},{','.join(self.other_words)}"
         
-    
-def get_bee(word):
-    has_7_letters = len(word) >= UNIQUE_LETTERS_COUNT and len(''.join(set(word))) == UNIQUE_LETTERS_COUNT
-    if has_7_letters:
-        other_words = set()
-        for w in english_words:
-            if w not in already_used and w != word and set(w).issubset(set(word)):
-                other_words.add(w)
-        if 10 <= len(other_words) <= 30:
-            return Bee(set(word), word, other_words)
-    return None
-    
-
-
-bees = []
-
-for word in english_words:
-    bee = get_bee(word)
-    if bee is not None:
-        bees.append(bee)
-        already_used.add(word)
-
+english_words = load_words()
+bees = Bee.create_bees(english_words)
 
 #fitting_words = list(filter(is_valid, english_words))
-print(f"{len(bees)} words with {UNIQUE_LETTERS_COUNT} unique letters:")
+#print(f"{len(bees)} words with {UNIQUE_LETTERS_COUNT} unique letters:")
 
 pydoc.pager('\n'.join(str(b) for b in bees))
 
