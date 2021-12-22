88e445e3d118b8a09ac524be816f40497cfbac20
jquentin-lion <jeremy.quentin@lionstudios.cc>
2021-12-22 15:14:42 +0100

    Paged result + alphabetical order

--- combfinder.py
+++ combfinder.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 import urllib.request
+import pydoc
 
 UNIQUE_LETTERS_COUNT = 7
 
 def load_words():
 #    urllib.request.urlretrieve("https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt", "words_alpha.txt")
     with open("words_alpha.txt") as word_file:
-        valid_words = set(word_file.read().split())
+        valid_words = word_file.read().split()
 
     return valid_words
     
 def is_valid(word):
-    return len(''.join(set(word))) == UNIQUE_LETTERS_COUNT
+    return len(word) >= UNIQUE_LETTERS_COUNT and len(''.join(set(word))) == UNIQUE_LETTERS_COUNT
     
 
 
@@ -21,5 +22,8 @@ if __name__ == '__main__':
     english_words = load_words()
     fitting_words = list(filter(is_valid, english_words))
     print(f"{len(fitting_words)} words with {UNIQUE_LETTERS_COUNT} unique letters:")
-    for i in range(0, 100):
-        print(f"{fitting_words[i]} -> {''.join(set(fitting_words[i]))}")
+
+    pydoc.pager('\n'.join(f"{word} -> {''.join(set(word))}" for word in fitting_words))
+
+#    for i in range(0, 100):
+#        print(f"{fitting_words[i]} -> {''.join(set(fitting_words[i]))}")
