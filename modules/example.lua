2cc3c53fed4f6b4eef8b9c449eecbe39ff05cb50
jquentin-lion <jeremy.quentin@lionstudios.cc>
2022-01-17 19:05:32 +0100

    Add more leaderboards

--- modules/example.lua
+++ modules/example.lua
@@ -2,4 +2,8 @@ local nk = require("nakama")
 
 nk.run_once(function(context)
   nk.leaderboard_create("all_time_points", false, "desc", "incr")
+  nk.leaderboard_create("all_time_points_en", false, "desc", "incr")
+  nk.leaderboard_create("all_time_points_fr", false, "desc", "incr")
+  nk.leaderboard_create("daily_points_en", false, "desc", "incr")
+  nk.leaderboard_create("daily_points_fr", false, "desc", "incr")
 end)
