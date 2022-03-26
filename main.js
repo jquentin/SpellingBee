bb58d7c9f4ad9b5341ba965299b80a7a09a8874c
jquentin-lion <jeremy.quentin@lionstudios.cc>
2022-03-26 19:31:55 +0100

    Fix clipboard

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ window.rpc = async function (id, input, callback)
 
 window.copyToClipboard = function(text)
 {
-    navigator.clipboard.writeText(text);
+    copyToClipboard(text);
 }
 
 var initTime;
@@ -179,5 +179,29 @@ window.connect_email = async function (email, password)
         {
             e.text().then(errMsg => { window.error += JSON.parse(errMsg).message + "\n"; console.log(window.error)});
         }
+    }
+}
+
+function copyToClipboard(textToCopy) {
+    // navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        // navigator clipboard api method'
+        return navigator.clipboard.writeText(textToCopy);
+    } else {
+        // text area method
+        let textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        // make the textarea out of viewport
+        textArea.style.position = "fixed";
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        return new Promise((res, rej) => {
+            // here the magic happens
+            document.execCommand('copy') ? res() : rej();
+            textArea.remove();
+        });
     }
 }
